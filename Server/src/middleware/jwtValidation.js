316acc8b3999333f2eac22ee0e936b84fd22a267
LuciusNguyen2303 <nhatccg456@gmail.com>
2024-04-26 14:34:26 +0700

    setting...

--- Server/src/middleware/jwtValidation.js
+++ Server/src/middleware/jwtValidation.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const {admin} = require('./Firebase/FirebaseConfig')
 require('dotenv').config()
 const authenticateToken = (req, res, next) => {
     const authorizationHeader = req.headers['authorization'];
@@ -29,35 +28,5 @@ const authenticateToken = (req, res, next) => {
         next();
     });
 }
-const authenticateTokenGG =  async (req, res, next) => {
-    const authorizationHeader = req.headers['authorization'];
-    if (authorizationHeader) {
-        if (authorizationHeader.startsWith('Bearer ')) {
-            console.log('Good to go');
-        } else {
-            console.log('Bad to go');
-        }
-    } else {
-        console.log('Cant move to headers or headers not valid');
-    }
 
-    const token = authorizationHeader.split(' ')[1];
-    
-    if (!token) {
-        console.log('Token is null'); 
-        return res.sendStatus(401)
-    } else {
-        console.log('token: ' + token);
-    }
-  const result= await admin.auth().verifyIdToken(token).then((decodedToken) => {
-    console.log(decodedToken.exp,decodedToken.auth_time,decodedToken.email);
-    next();
-    // ...
-  })
-  .catch((error) => {
-    // Handle error
-    console.log(error)
-  })
-  next();
-}
-module.exports = { authenticateToken,authenticateTokenGG };
+module.exports = { authenticateToken };
